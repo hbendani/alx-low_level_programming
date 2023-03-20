c601b3e20fd42130e5f0b6093033cc1691bdc4e8
hbendani <hbendani@yahoo.fr>
2023-03-20 14:06:16 -0700

    optional

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a text wihtout using printf
+ * Return: 0
+ */
+int main(void)
+{
+	char texte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, texte, str_length(texte));
+	return (0);
+}
