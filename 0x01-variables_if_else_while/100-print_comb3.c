709351acda706bee1c88620e697711f8103db2f0
hbendani <hbendani@yahoo.fr>
2023-03-20 15:13:41 -0700

    print com 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all possible comb of two digit
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 8 ; i++)
+	{
+		for (int j = 0 ; j <= 9 ; j++)
+		{
+			if (j > i)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+				if (i < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
