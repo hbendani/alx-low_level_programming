87c0f4a5f8fb017a178dfbad3ce0c96b72316fe6
hbendani <hbendani@yahoo.fr>
2023-03-20 15:35:58 -0700

    com of 3 digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/** 
+ * main - print all com of 3 digit
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, z;
+
+	for (i = 0 ; i <= 8 ; i++)
+	{
+		for (j = 0 ; j <= 8 ; j++)
+		{
+			if (j > i)
+			{
+				for (z = 0 ; z < 10 ; z++)
+				{
+					if (z > j)
+					{
+						putchar(i%10 +'0');
+						putchar(j%10 + '0');
+						putchar(z%10 + '0');
+						if (i < 7)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
