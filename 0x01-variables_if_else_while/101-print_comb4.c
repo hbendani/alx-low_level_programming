d6f80839215784a61b87ec34146597a84d1f74eb
hbendani <hbendani@yahoo.fr>
2023-03-20 15:44:11 -0700

    com of 3 digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,26 +8,18 @@ int main(void)
 	int i, j, z;
 
 	for (i = 0 ; i <= 8 ; i++)
+	for (j = 0 ; j <= 8 ; j++)
+	if (j > i)
+	for (z = 0 ; z < 10 ; z++)
+	if (z > j)
 	{
-		for (j = 0 ; j <= 8 ; j++)
+		putchar(i % 10 + '0');
+		putchar(j % 10 + '0');
+		putchar(z % 10 + '0');
+		if (i < 7)
 		{
-			if (j > i)
-			{
-				for (z = 0 ; z < 10 ; z++)
-				{
-					if (z > j)
-					{
-						putchar(i % 10 + '0');
-						putchar(j % 10 + '0');
-						putchar(z % 10 + '0');
-						if (i < 7)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
