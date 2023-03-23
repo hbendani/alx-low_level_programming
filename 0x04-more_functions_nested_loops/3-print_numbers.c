c9c4ce72f01f58d773f6b11f18f5399679886456
hbendani <hbendani@yahoo.fr>
2023-03-23 13:21:59 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include <main.h>
 /**
  * print_numbers - print numbers
- * Return: void
+ * Return: 0
  */
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-	_putchar(i + '0');
+	_putchar(i);
 	_putchar('\n');
 }
