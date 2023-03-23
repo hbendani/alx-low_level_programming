26733cca599b16a5196ab94160072fea4455a3dc
hbendani <hbendani@yahoo.fr>
2023-03-23 03:12:35 -0700

    second

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include <stdio.h>
 /**
  * print_alphabet_x10 - prints 10 times the alphabet
  * Return: void
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	{
 	for (i = 'a'; i <= 'z'; i++)
 	putchar(i);
-	putchar("\n");
+	putchar('\n');
 	n++;
 	}
 }
