fdbe0ae06269189cd274e90477cc1a1385340d3c
hbendani <hbendani@yahoo.fr>
2023-04-10 14:35:00 -0700

    all

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: nbr of arguments
+ * @argv: arra of all the arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
